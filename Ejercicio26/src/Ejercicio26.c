60e90bfe9f9823834b9e97862b61269149f572fd
Kevin Quevedo <Kquevedo@prismamp.com>
2020-04-10 23:09:34 -0300

    Extras1

---
+++ Ejercicio26/src/Ejercicio26.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Name        : Ejercicio26.c
+ Author      : Kevin Quevedo
+ Version     : 1.0
+ Folder		 : TrabajosExtras
+ Copyright   : Your copyright notice
+ Description : Hacer el programa ante la pregunta desea continuar sólo nos
+ 	 	 	   permita introducir como respuesta 'S' o'N'
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+
+
+	return EXIT_SUCCESS;
+}
